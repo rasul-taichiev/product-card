254de6468f2d1b864f2954f3c0607757958a8013
rasul-taichiev <taychiev@gmail.com>
2026-08-08 16:11:43 +0600

    fixed format

--- main.js
+++ main.js
@@ -1,61 +1,58 @@
 //1
-const productCards = document.querySelectorAll('.card');
-const changeColorAllCard = document.querySelector('#change-color-all-card');
-const darkGreenColorHash ='#006400';
-const redColorRGB ='rgb(139, 0, 0)';
+const productCards = document.querySelectorAll(".card");
+const changeColorAllCard = document.querySelector("#change-color-all-card");
+const darkGreenColorHash = "#006400";
+const redColorRGB = "rgb(139, 0, 0)";
 
-changeColorAllCard.addEventListener('click', () => {
-  productCards.forEach((card) => card.style.background = darkGreenColorHash);
+changeColorAllCard.addEventListener("click", () => {
+  productCards.forEach((card) => (card.style.background = darkGreenColorHash));
 });
 //2
-const firstProductCard = document.querySelector('.card');
-const changeFirstCard = document.querySelector('#changeFirstCard');
+const firstProductCard = document.querySelector(".card");
+const changeFirstCard = document.querySelector("#changeFirstCard");
 
-changeFirstCard.addEventListener('click' , () => {
-    firstProductCard.style.background = redColorRGB
+changeFirstCard.addEventListener("click", () => {
+  firstProductCard.style.background = redColorRGB;
 });
 //3
-const goToGoogleButton = document.querySelector('#goto-google');
+const goToGoogleButton = document.querySelector("#goto-google");
 
-goToGoogleButton.addEventListener('click' , openGoogle)
-
+goToGoogleButton.addEventListener("click", openGoogle);
 
 function openGoogle() {
-    const answer = confirm('Вы действительно хотите открыть Google?');
+  const answer = confirm("Вы действительно хотите открыть Google?");
 
-    if(answer === true) {
-        window.open('https://google.com')
-    }else{
-        return;
-    }
+  if (answer === true) {
+    window.open("https://google.com");
+  } else {
+    return;
+  }
 }
 
 //4
-const outputLogButton = document.querySelector('#output-console-log');
+const outputLogButton = document.querySelector("#output-console-log");
 
-outputLogButton.addEventListener('click',
-    () => outputConsoleLog('привет мир'))
+outputLogButton.addEventListener("click", () => outputConsoleLog("привет мир"));
 
 function outputConsoleLog(message) {
-    alert('+99999999999')
-    console.log(message)
-};
+  alert("+99999999999");
+  console.log(message);
+}
 
 //5
-const hoverOverTitle = document.querySelector('.title');
+const hoverOverTitle = document.querySelector(".title");
 
-hoverOverTitle.addEventListener('mouseover',
-    () => outputConsoleTitle(hoverOverTitle) )
+hoverOverTitle.addEventListener("mouseover", () =>
+  outputConsoleTitle(hoverOverTitle),
+);
 
 function outputConsoleTitle(message) {
-    console.log(message)
-};
+  console.log(message);
+}
 
 //6
-const changeColorLastButton = document.querySelector('#buttonChanger')
+const changeColorLastButton = document.querySelector("#buttonChanger");
 
-changeColorLastButton.addEventListener('click', () => {
-    changeColorLastButton.classList.toggle('active')
-})
-
-
+changeColorLastButton.addEventListener("click", () => {
+  changeColorLastButton.classList.toggle("active");
+});
