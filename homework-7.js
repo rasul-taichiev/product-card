0e10709375ee73162b9894eb57f172cdef4ae1d4
rasul-taichiev <taychiev@gmail.com>
2026-08-15 14:46:10 +0600

    Used objects and array

--- homework-7.js
+++ homework-7.js
@@ -1,7 +1,7 @@
 const getCityAmdTemperature = (city, temperature) => {
-  console.log(
-    `Сейчас в ${city} температура -- ${temperature} градусов по Цельсию`,
-  );
+    console.log(
+        `Сейчас в ${city} температура -- ${temperature} градусов по Цельсию`,
+    );
 };
 
 getCityAmdTemperature("Bishkek", "38");
@@ -9,13 +9,13 @@ getCityAmdTemperature("Bishkek", "38");
 //2
 const lightSpeed = 299792458;
 const getSpeed = (speed) => {
-  if (speed > lightSpeed) {
-    console.log("Сверхсветовая скорость");
-  } else if (speed === lightSpeed) {
-    console.log("Скорость света");
-  } else {
-    console.log("Субсветовая скорость");
-  }
+    if (speed > lightSpeed) {
+        console.log("Сверхсветовая скорость");
+    } else if (speed === lightSpeed) {
+        console.log("Скорость света");
+    } else {
+        console.log("Субсветовая скорость");
+    }
 };
 
 getSpeed(12983838);
@@ -24,12 +24,12 @@ getSpeed(12983838);
 const product = "Cheaps";
 const price = 200;
 const buyProduct = (budget) => {
-  if (budget > price) {
-    console.log(`${product} приобретён. Спасибо за покупку!`);
-  } else {
-    const lackOfMoney = price - budget;
-    console.log(`Вам не хватает ${lackOfMoney}$, пополните баланс`);
-  }
+    if (budget > price) {
+        console.log(`${product} приобретён. Спасибо за покупку!`);
+    } else {
+        const lackOfMoney = price - budget;
+        console.log(`Вам не хватает ${lackOfMoney}$, пополните баланс`);
+    }
 };
 buyProduct(180);
 
@@ -39,20 +39,31 @@ const earnBonus = 1000;
 const accumulation = 70000;
 
 const buyHouse = (price, percent = 10) => {
-  const totalBudget = earns + earnBonus + accumulation;
-  if (totalBudget >= price) {
-    console.log("Вы можете купить дом за вашу зарплату и накопления.");
-  } else if (totalBudget < price) {
-    const markup = price * (percent / 100); // Наценка
-    const priceWithMarkup = price + markup;
-    const lackOfBudget = priceWithMarkup - totalBudget;
-    const earnPerMonth = earns + earnBonus;
-    const payPerMonth = Math.ceil(lackOfBudget / earnPerMonth);
-    console.log(
-      `Цена дома: ${price}. Наценка (${percent}%): ${markup}$. Итого: ${priceWithMarkup}$.`,
-    );
-    console.log(`Вы можете купить дом в рассрочку на ${payPerMonth} месяца.`);
-  }
+    const totalBudget = earns + earnBonus + accumulation;
+    if (totalBudget >= price) {
+        console.log("Вы можете купить дом за вашу зарплату и накопления.");
+    } else if (totalBudget < price) {
+        const markup = price * (percent / 100); // Наценка
+        const priceWithMarkup = price + markup;
+        const lackOfBudget = priceWithMarkup - totalBudget;
+        const earnPerMonth = earns + earnBonus;
+        const payPerMonth = Math.ceil(lackOfBudget / earnPerMonth);
+        console.log(
+            `Цена дома: ${price}. Наценка (${percent}%): ${markup}$. Итого: ${priceWithMarkup}$.`,
+        );
+        console.log(
+            `Вы можете купить дом в рассрочку на ${payPerMonth} месяца.`,
+        );
+    }
 };
 
 buyHouse(200000);
+
+function sayHi() {
+    console.log("Привет!");
+}
+
+let result = sayHi();
+console.log(result);
+
+sayHi();
